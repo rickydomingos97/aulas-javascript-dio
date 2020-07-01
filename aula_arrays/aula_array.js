a6fa78fc88400a011e437657a1af6094ab024f01
iricardo97 <ricardodomingos97@gmail.com>
2020-07-01 16:10:21 -0300

    dictionary and arrays add to aula_array.js

--- aula_arrays/aula_array.js
+++ aula_arrays/aula_array.js
@@ -1,6 +1,11 @@
+var fruit = [{name: "apple", color: "red"}, {name: "pinaple", color: "green"}];
+console.log(fruit);
+console.log(fruit[1].name);
+/* 
 var fruit = {name: "apple", color: "red"}
+console.log(fruit.color);
 
-/* var list = ["apple", "pera", "banana"];
+var list = ["apple", "pera", "banana"];
 list.push("grape");
 list.pop();
 console.log(list.length);
