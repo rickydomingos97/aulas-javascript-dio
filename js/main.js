723eb47f3a86fca54a280a150b7e50001cdd7c98
iricardo97 <ricardodomingos97@gmail.com>
2020-07-01 07:54:36 -0300

    replace text add to main,js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 var name = "Ricardo Domingos";
-var age = "38";
-alert(name + " is " + age + " years old");
+var age = 38;
+var age2 = 10;
+var phrase = "Japan is the best team ever"
+//alert(name + " is " + age + " years old");
+//You can make math operations on js
+//alert(age + age2);
+console.log(name);
+console.log(age);
+console.log(age + age2);
+console.log(phrase);
+console.log(phrase.replace("Japan", "Brazil"))
