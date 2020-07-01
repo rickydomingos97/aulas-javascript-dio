df616a1ac1f98798c7dfecfad0ad02e6478fb962
iricardo97 <ricardodomingos97@gmail.com>
2020-07-01 14:03:19 -0300

    "fruitdictionary created"

--- js/aula_array.js
+++ js/aula_array.js
@@ -1,2 +1,12 @@
-var list = ["apple", "pera", "banana"];
-console.log(list[1]);
+var fruit = {name: "apple", color: "red"}
+
+/* var list = ["apple", "pera", "banana"];
+list.push("grape");
+list.pop();
+console.log(list.length);
+console.log(list.reverse());
+console.log(list[0])
+console.log(list.toString()[0]);
+console.log(list.join(" | "));
+alert(list[2]);
+*/
