dace769afb567fd643f50f8cab995a55ff70cc78
iricardo97 <ricardodomingos97@gmail.com>
2020-07-01 08:01:33 -0300

    add math actions and toUpperCase() to main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var name = "Ricardo Domingos";
 var age = 38;
 var age2 = 10;
+var n1 = 5;
+var n2 = 3;
 var phrase = "Japan is the best team ever"
 //alert(name + " is " + age + " years old");
 //You can make math operations on js
@@ -8,5 +10,6 @@ var phrase = "Japan is the best team ever"
 console.log(name);
 console.log(age);
 console.log(age + age2);
-console.log(phrase);
-console.log(phrase.replace("Japan", "Brazil"))
+console.log(phrase.toUpperCase());
+console.log(phrase.replace("Japan", "Brazil").toUpperCase())
+console.log(n1 * n2);
