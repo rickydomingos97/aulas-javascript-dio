7cd70068485e533feca576b98a79cc5e5c813521
iricardo97 <ricardodomingos97@gmail.com>
2020-07-02 13:27:19 -0300

    add function setReplace to function.js

--- function/function.js
+++ function/function.js
@@ -1,4 +1,10 @@
-function soma(n1, n2){
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+console.log(setReplace("Go Japan", "Japan", "Brasil"))
+/*function soma(n1, n2){
     return n1 + n2;
 }
-console.log(soma(5, 2));
+console.log(soma(5, 2));*/
+
