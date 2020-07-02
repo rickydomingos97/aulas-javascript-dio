36245248eb05a16234554bd037791b6e9bc4988c
iricardo97 <ricardodomingos97@gmail.com>
2020-07-02 13:39:44 -0300

    validar idade add to funcion.js

--- function/function.js
+++ function/function.js
@@ -1,10 +1,25 @@
+
+/*
 function setReplace(frase, nome, novo_nome){
     return frase.replace(nome, novo_nome)
 }
 
 console.log(setReplace("Go Japan", "Japan", "Brasil"))
-/*function soma(n1, n2){
+
+function soma(n1, n2){
     return n1 + n2;
 }
 console.log(soma(5, 2));*/
+var validar = 0;
 
+function validarIdade(idade){
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validarIdade(idade));
